efa2bccd569169291e32b0b81ac6d9a86b6bda0d
UttkarrshhPal <palutkarsh1901@gmail.com>
2024-08-03 11:39:06 +0530

    02

--- 02.sql
+++ 02.sql
@@ -1,53 +1,55 @@
-DROP TABLE IF EXISTS products;
-CREATE TABLE products(
-   product_id INT PRIMARY KEY,
-   product_name VARCHAR(50),
-   category VARCHAR(50),
-   quantity_sold INT
+CREATE TABLE Orders(
+  OrderID INT PRIMARY KEY,
+  OrderDate DATE,
+  TotalAmount DECIMAL(10,2)
 );
+INSERT INTO Orders(OrderID,OrderDate,TotalAmount) VALUES
+(1, '2023-01-15', 150.50),
+(2, '2023-02-20', 200.75),
+(3, '2023-02-28', 300.25),
+(4, '2023-03-10', 180.00),
+(5, '2023-04-05', 250.80);
 
-INSERT INTO products (product_id,product_name,category,quantity_sold) VALUES
-(1, 'Samsung Galaxy S20', 'Electronics', 100),
-(2, 'Apple iPhone 12 Pro', 'Electronics', 150),
-(3, 'Sony PlayStation 5', 'Electronics', 80),
-(4, 'Nike Air Max 270', 'Clothing', 200),
-(5, 'Adidas Ultraboost 20', 'Clothing', 200),
-(6, 'Levis Mens 501 Jeans', 'Clothing', 90),
-(7, 'Instant Pot Duo 7-in-1', 'Home & Kitchen', 180),
-(8, 'Keurig K-Classic Coffee Maker', 'Home & Kitchen', 130),
-(9, 'iRobot Roomba 675 Robot Vacuum', 'Home & Kitchen', 130),
-(10, 'Breville Compact Smart Oven', 'Home & Kitchen', 90),
-(11, 'Dyson V11 Animal Cordless Vacuum', 'Home & Kitchen', 90);
+CREATE TABLE Returns(
+  ReturnID INT PRIMARY KEY,
+  OrderID INT,
+  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+  );
 
--- Write a SQL query to find top selling products in each category 
-SELECT *
-FROM 
-(SELECT *,
-DENSE_RANK() OVER(PARTITION BY category ORDER BY quantity_sold DESC) rnk
-FROM products) x
-WHERE x.rnk = 1;
+INSERT INTO Returns (ReturnID, OrderID) VALUES
+(101, 2),
+(102, 4),
+(103, 5),
+(104, 1),
+(105, 3);
 
+SELECT * FROM Orders;
+SELECT * FROM Returns;
 
--- Method 2 
-SELECT *
-FROM 
-(	
-	SELECT 
-		*,
-		RANK() OVER(PARTITION BY category ORDER BY quantity_sold DESC) ranks
-	FROM products
-	ORDER BY category, quantity_sold DESC
-) as subquery	
-WHERE ranks = 1;
+-- Write a SQL Query to calculate total Number of Returned Items Each Months
 
--- Write a SQL query to find least-selling products in each category 
-SELECT * 
-FROM 
-(
-SELECT *,
-RANK() OVER(PARTITION BY category ORDER BY quantity_sold) rnk
-FROM products
-ORDER BY quantity_sold,category
-) X
-WHERE x.rnk=1;
+-- Approach order_id LEFT JOIN
+-- Order by date
+-- Gruop by date
 
+SELECT
+   DATE_FORMAT(o.OrderDate,'%Y-%M') AS OrderMonth,
+   COUNT(r.ReturnID) AS TotalReturns
+FROM Orders o
+LEFT JOIN Returns r
+ON o.OrderID = r.OrderID
+GROUP BY o.OrderDate
+ORDER BY o.OrderDate;
+
+-- Write a SQL Query to calculate total number of orders retrned B/w any  2 months
+-- Lets suppose b/w jan to april 
+
+SELECT
+  DATE_FORMAT(o.OrderDate,'%Y-%M') AS OrderMonth,
+  COUNT(r.ReturnID) AS TotalReturns
+FROM Orders o
+LEFT JOIN Returns r
+ON o.OrderID = r.OrderID
+WHERE o.OrderDate BETWEEN '2023-01-01' AND '2023-04-30'
+GROUP BY o.OrderDate
+ORDER BY o.OrderDate 
